186aeaaac12aa44fbeacb155674944d313fc7fce
Alfonso <alfonsoscudiero@gmail.com>
2024-04-26 22:47:10 -0600

    Working with loops and random numbers

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number?: ");
+        // string magicNumber = Console.ReadLine();
+        // int number = int.Parse(magicNumber);
+
+        // Part 3, use a random number
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+
+        int guess = 0;
+
+        while (guess != number)
+        {
+            Console.Write("What is your guess?: ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
