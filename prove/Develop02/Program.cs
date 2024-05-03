e92e6a787dfab807d955fb8fb1a9325a33737d43
Alfonso <alfonsoscudiero@gmail.com>
2024-05-02 20:18:24 -0600

    Started the Program file

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,17 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        Entry anEntry = new Entry();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        Console.WriteLine('Welcome to My Journal App!');
+        Console.WriteLine('Please select one of the following choices:')
+
+
     }
 }
