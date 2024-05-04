6b2daab6e0fe6fd13f7429d6344447d3bb4c8ff1
Alfonso <alfonsoscudiero@gmail.com>
2024-05-04 11:35:40 -0600

    Add list of prompts and the method to get them ramdomly

--- prove/Develop02/PromptGenerator.cs
+++ prove/Develop02/PromptGenerator.cs
@@ -1,10 +1,20 @@
 using System;
 public class PromptGenerator
 {
-    public List<string> _prompts;
+    public List<string> _prompts = new List<string>
+    {
+        "Describe a challenge you faced today.",
+        "What are you grateful for today?",
+        "If you could travel anywhere in the world, where would you go?",
+        "What makes you happy?",
+        "Five years from now, I will be...",
+        "List the top 3 people you admire, and why."
+    };
+    public Random randomPrompt = new Random();
+    // Method to get random prompt from the list
     public string GetRandomPrompt()
     {
-        return "";
+        int index = randomPrompt.Next(_prompts.Count);
+        return _prompts[index];
     }
-
 }
