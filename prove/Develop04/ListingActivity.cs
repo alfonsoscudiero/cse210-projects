4f552662ab61900df225d3b612bd29d13b645a8c
Alfonso <alfonsoscudiero@gmail.com>
2024-05-16 23:55:54 -0600

    Started the ListingActivity child class

---
+++ prove/Develop04/ListingActivity.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+public class ListingActivity
+{
+    private int _count;
+    private List<string> _prompts;
+    // Constructor
+    public ListingActivity()
+    {
+
+    }
+    // Method to
+    public void Run()
+    {
+
+    }
+    // Method to
+    public void GetRandomPrompt()
+    {
+
+    }
+    // Method to
+    public List<string> GetListFromUser()
+    {
+        return null;
+    }
+}
