ff6c5c21e1bcf5c16c5acd2869c7cbc567d36ed5
Alfonso <alfonsoscudiero@gmail.com>
2024-05-16 23:56:11 -0600

    Started the ReflectingActivity child class

---
+++ prove/Develop04/ReflectingActivity.cs
@@ -0,0 +1,35 @@
+using System;
+
+public class ReflectingActivity
+{
+    private List<string> _prompts;
+    private List<string> _questions;
+    // Constructor
+    public ReflectingActivity()
+    {
+
+    }
+    // Method to
+    public void Run()
+    {
+
+    }
+    // Method to 
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+    // Method to 
+    public string GetRandomQuestion()
+    {
+        return "";
+    }
+    public void DisplayPrompt()
+    {
+
+    }
+    public void DisplayQuestions()
+    {
+
+    }
+}
